29301a5866199d718b0d104db8d8d0b9bffd0f43
MrVtR <30987772+MrVtR@users.noreply.github.com>
2023-02-13 23:50:49 -0300

    Versão nova doa algoritmos

--- Upscalling/Auto_upscaller/upscalling_auto.py
+++ Upscalling/Auto_upscaller/upscalling_auto.py
@@ -1,33 +1,36 @@
 import cv2
 import matplotlib.pyplot as plt
 import utils
-# Read image
-img = cv2.imread("0001_20_face.jpg")
 
-# Cropout OpenCV logo
-# img = img[:120,910:]
+# files = ['branco','black','asia','indian','latino','oriente']
 
-sr = cv2.dnn_superres.DnnSuperResImpl_create()
-path = "nchw_frozen_ESPCN_graph_x4.pb"
-sr.readModel(path)
-sr.setModel("espcn",4)
-result = sr.upsample(img)
- 
-# Resized image
-resized = cv2.resize(img,dsize=None,fx=4,fy=4)
+files=['black']
 
-print("PSNR of ESPCN generated image em relação a imagem original em Resize: ", utils.PSNR(resized, result))
+for file in files:
+  img = cv2.imread(file+".jpg")
+
+  sr = cv2.dnn_superres.DnnSuperResImpl_create()
+  path = "asian_nchw_frozen_ESPCN_graph_x4.pb"
+  sr.readModel(path)
+  sr.setModel("espcn",4)
+  result = sr.upsample(img)
+  
+  # Resized image
+  resized = cv2.resize(img,dsize=None,fx=4,fy=4)
+
+  print("Raça:",file,"- PSNR of ESPCN generated image em relação a imagem original em Resize: ", utils.PSNR(resized, result))
+  print()
 
 
-plt.figure(figsize=(6,5))
-plt.subplot(1,1,1)
-plt.imshow(img[:,:,::-1])
+  plt.figure(figsize=(6,5))
+  plt.subplot(1,1,1)
+  plt.imshow(img[:,:,::-1])
 
-plt.figure(figsize=(6,5))
-plt.subplot(1,1,1)
-plt.imshow(result[:,:,::-1])
+  plt.figure(figsize=(6,5))
+  plt.subplot(1,1,1)
+  plt.imshow(result[:,:,::-1])
 
-plt.figure(figsize=(6,5))
-plt.subplot(1,1,1)
-plt.imshow(resized[:,:,::-1])
-plt.show()
+  plt.figure(figsize=(6,5))
+  plt.subplot(1,1,1)
+  plt.imshow(resized[:,:,::-1])
+  plt.show()
