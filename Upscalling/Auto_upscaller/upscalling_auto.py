7247d70a1870e2187e956dc4a26f998cba6c4070
MrVtR <30987772+MrVtR@users.noreply.github.com>
2023-03-20 14:31:57 -0300

    Arquivos da fase final adicionados

--- Upscalling/Auto_upscaller/upscalling_auto.py
+++ Upscalling/Auto_upscaller/upscalling_auto.py
@@ -2,35 +2,34 @@ import cv2
 import matplotlib.pyplot as plt
 import utils
 
-# files = ['branco','black','asia','indian','latino','oriente']
+imageTestFiles=['asiatico','branco','indiano','latino','negro','oriente']
+CNNFolders = ['FSRCNN','TF-ESPCN','TF-LapSRN']
+modelsFolders = ['asian','black','indian','latino','middle','mixed','white']
 
-files=['black']
+for file in imageTestFiles:
+  img = cv2.imread("images/"+file+".jpg")
 
-for file in files:
-  img = cv2.imread(file+".jpg")
+  for model in modelsFolders:
+    print("Modelo:",model)
+    sr = cv2.dnn_superres.DnnSuperResImpl_create()
+    #Path para o modelo FSRCNN
+    path = "./models/FSRCNN/"+model+"/FSRCNN_x2.pb"
 
-  sr = cv2.dnn_superres.DnnSuperResImpl_create()
-  path = "asian_nchw_frozen_ESPCN_graph_x4.pb"
-  sr.readModel(path)
-  sr.setModel("espcn",4)
-  result = sr.upsample(img)
-  
-  # Resized image
-  resized = cv2.resize(img,dsize=None,fx=4,fy=4)
+    #Path para os modelos TF-ESPCN
+    # path = './models/TF-ESPCN'+model+'frozen_ESPCN_graph_x4.pb'
 
-  print("Raça:",file,"- PSNR of ESPCN generated image em relação a imagem original em Resize: ", utils.PSNR(resized, result))
-  print()
+    #Path para os modelos TF-Lap-SRN
+    # path = './models/TF-LapSRN'+'LapSRN_x2.pb'
+    sr.readModel(path)
+    sr.setModel("fsrcnn",2)
+    result = sr.upsample(img)
+    
+    # Resized image
+    resized = cv2.resize(img,dsize=None,fx=2,fy=2)
+
+    print("Raça:",file,"- PSNR:", utils.PSNR(resized, result))
+    print()
+
+  print("--------------------------------------------------------")
 
 
-  plt.figure(figsize=(6,5))
-  plt.subplot(1,1,1)
-  plt.imshow(img[:,:,::-1])
-
-  plt.figure(figsize=(6,5))
-  plt.subplot(1,1,1)
-  plt.imshow(result[:,:,::-1])
-
-  plt.figure(figsize=(6,5))
-  plt.subplot(1,1,1)
-  plt.imshow(resized[:,:,::-1])
-  plt.show()
